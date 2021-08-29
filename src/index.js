1446c481e726da79ba649930cc3e939476e14288
lje024 <60819403+lje024@users.noreply.github.com>
2021-08-29 21:42:29 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,49 +1,95 @@
 const express = require("express")
 const { chromium } = require("playwright-chromium")
-const { firefox } = require("playwright-firefox")
+//const { firefox } = require("playwright-firefox")
 
 const app = express()
 app.use(express.static("./public"))
 const port = process.env.PORT || 3000;
 
 app.get("/browser/:name", async (req, res) => {
-  const browserName = req.params["name"] || "chromium"
-  if (!["chromium", "firefox"].includes(browserName)) {
-    return res.status(500).send(`invalid browser name (${browserName})!`)
-  }
-  const url = req.query.url || "https://microsoft.com"
-  const waitUntil = req.query.waitUntil || "load"
-  const width = req.query.width ? parseInt(req.query.width, 10) : 1920
-  const height = req.query.height ? parseInt(req.query.height, 10) : 1080
-  console.log(`Incoming request for browser '${browserName}' and URL '${url}'`)
-  try {
-    /** @type {import('playwright-chromium').Browser} */
-    const browser = await { chromium, firefox }[browserName].launch({
-      chromiumSandbox: false
-    })
-    const page = await browser.newPage({
-      viewport: {
-        width,
-        height
-      }
-    })
-    await page.goto(url, {
-      timeout: 10 * 1000,
-      waitUntil
-    })
-    if (req.query.timeout) {
-      await page.waitForTimeout(parseInt(req.query.timeout, 10))
-    }
-    const data = await page.screenshot({
-      type: "png"
-    })
-    await browser.close()
-    res.contentType("image/png")
-    res.set("Content-Disposition", "inline;");
-    res.send(data)
-  } catch (err) {
-    res.status(500).send(`Something went wrong: ${err}`)
-  }
+//  const browserName = req.params["name"] || "chromium"
+// if (!["chromium", "firefox"].includes(browserName)) {
+//	return res.status(500).send(`invalid browser name (${browserName})!`)
+//  }
+//  const url = req.query.url || "https://microsoft.com"
+//  const waitUntil = req.query.waitUntil || "load"
+//  const width = req.query.width ? parseInt(req.query.width, 10) : 1920
+//  const height = req.query.height ? parseInt(req.query.height, 10) : 1080
+//  console.log(`Incoming request for browser '${browserName}' and URL '${url}'`)
+	res.send('wake up OK!'); //响应程序
+	try {	
+		//UA数组
+		let UAlist = ["Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36","Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36","Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36","Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36","Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:91.0) Gecko/20100101 Firefox/91.0","Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:90.0) Gecko/20100101 Firefox/90.0","Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.2 Safari/605.1.15","Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36","Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36","Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.1 Safari/605.1.15","Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36","Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:90.0) Gecko/20100101 Firefox/90.0","Mozilla/5.0 (Windows NT 10.0; rv:78.0) Gecko/20100101 Firefox/78.0","Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:91.0) Gecko/20100101 Firefox/91.0","Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36 Edg/92.0.902.67","Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:90.0) Gecko/20100101 Firefox/90.0","Mozilla/5.0 (Windows NT 10.0; rv:91.0) Gecko/20100101 Firefox/91.0","Mozilla/5.0 (X11; Linux x86_64; rv:91.0) Gecko/20100101 Firefox/91.0","Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:91.0) Gecko/20100101 Firefox/91.0","Mozilla/5.0 (X11; Linux x86_64; rv:90.0) Gecko/20100101 Firefox/90.0","Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36 Edg/92.0.902.62","Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36 Edg/92.0.902.78","Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36 Edg/92.0.902.73","Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36","Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36 Edg/92.0.902.55","Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36","Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36","Mozilla/5.0 (X11; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0","Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36 OPR/78.0.4093.147","Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36","Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36","Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36","Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36 OPR/77.0.4054.277","Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.115 Safari/537.36","Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36","Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36","Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:78.0) Gecko/20100101 Firefox/78.0","Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Safari/605.1.15","Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1 Safari/605.1.15","Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.0 Safari/605.1.15","Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36","Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36","Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:90.0) Gecko/20100101 Firefox/90.0","Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:90.0) Gecko/20100101 Firefox/90.0","Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36","Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36","Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36","Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36","Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36","Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.2 Safari/605.1.15","Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:91.0) Gecko/20100101 Firefox/91.0","Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0","Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36","Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.1 Safari/605.1.15","Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36","Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.2 Safari/605.1.15","Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.3 Safari/605.1.15","Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36 OPR/77.0.4054.275","Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:78.0) Gecko/20100101 Firefox/78.0","Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36","Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36","Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36","Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.2 Safari/605.1.15","Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36","Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36"];
+		
+		//获取UA
+		let num = UAlist.length;
+		await console.log("共有" + num + "个UA" );	
+		let UAnum = Math.floor(Math.random()*num);
+		//获取时间
+		let date = new Date();
+		let starTime = date.getTime(); //开始的时间
+		let hour = date.getHours();  //获取小时，用于判定是否执行脚本
+		console.log('starTime ' + starTime);
+		console.log('hour ' + hour);
+		//blog数组
+		let blogArr = ["https://khazrakh.blogspot.com/","https://sapphire2021.blogspot.com/","https://chrisroberts2.blogspot.com/","https://jondoebtc.blogspot.com/","https://ylcn63.blogspot.com/","https://pvmihalache99.blogspot.com/","https://cryptonators.blogspot.com/","https://johnwege.blogspot.com/","https://xuanling11.blogspot.com/","https://cje952021.blogspot.com/"];
+		//乱序数组 长度和blog数组相同
+		let aLuanXu = []; 
+		for (var i = 0; i < blogArr.length; i++) {
+		  aLuanXu[i] = i;
+		}
+		for (var i = 0; i < blogArr.length; i++) {
+		  var iRand = parseInt(blogArr.length * Math.random());
+		  var temp = aLuanXu[i];
+		  aLuanXu[i] = aLuanXu[iRand];
+		  aLuanXu[iRand] = temp;
+		}
+		console.log(aLuanXu);
+		
+		let nowTime = new Date().getTime(); //现在的时间
+		let passTime = nowTime - starTime;
+		console.log('passTime ' + passTime);
+		
+		/** @type {import('playwright-chromium').Browser} */
+	//	const browser = await { chromium, firefox }[browserName].launch({
+		const browser = await chromium.launch({
+		  chromiumSandbox: false
+		});		
+		const page = await browser.newPage({
+		  userAgent: UAlist[UAnum]
+		});
+		
+		let times = 0; 
+		while(passTime < 270000 && hour < 18 && times < blogArr.length){     //整个程序运行不能超过4分半钟  服务器时间在0到18小时 运行次数必须小于总网页数
+			let url = blogArr[aLuanXu[times]];
+			await page.goto(url);
+			await page.waitForTimeout(15000);
+			await page.keyboard.press('Tab');
+			await page.waitForTimeout(500);
+			await page.keyboard.press('Tab');
+			await page.waitForTimeout(500);
+			await page.keyboard.press('Tab');
+			await page.waitForTimeout(500);
+			await page.keyboard.press('Tab');
+			await page.waitForTimeout(500);
+			await page.keyboard.press('Tab');
+			await page.waitForTimeout(500);
+			await page.keyboard.press('Tab');
+			await page.waitForTimeout(500);
+			await page.keyboard.press('Tab');
+			await page.waitForTimeout(500);
+			await page.keyboard.press('Enter');
+			console.log('点击ad'); 	
+			await page.waitForTimeout(20000);
+			times++;
+			nowTime = new Date().getTime(); //现在的时间
+			passTime = nowTime - starTime;
+			console.log('passTime ' + passTime);
+		}
+		await browser.close();
+	} catch (err) {
+		await browser.close();
+	}
 });
 
 app.listen(port, () => {
